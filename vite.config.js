9556dc2417d949a2b5c3d7eb581ace3c3735e810
Simon Auer <simon.auer@marqably.com>
2025-04-08 18:27:51 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react' // Or your framework plugin
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // plugins: [react()], // Or your framework plugin
+  base: '/flites-website/', // <--- Updated repository name
   server: {
     open: true, // Automatically open the browser
     port: 3000
