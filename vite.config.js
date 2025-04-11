6b7a4b36a807ca703510d846af3917d014724f6b
Simon Auer <simon.auer@marqably.com>
2025-04-11 17:46:24 +0200

    Split header and footer up in reusable partials

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
+import htmlInject from 'vite-plugin-html-inject' // <-- Change to default import
 // import react from '@vitejs/plugin-react' // Or your framework plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    htmlInject() // <-- This should now work
+  ],
   // plugins: [react()], // Or your framework plugin
-  base: '/flites-website/', // <--- Updated repository name
+  base: '/', // <--- Updated repository name
   server: {
     open: true, // Automatically open the browser
     port: 3000
