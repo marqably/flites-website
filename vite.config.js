16555b93c282a6dee71e812d3afca8ca7f32e022
Simon Auer <simon.auer@marqably.com>
2025-04-14 09:30:51 +0200

    Add subpages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import htmlInject from 'vite-plugin-html-inject' // <-- Change to default import
 import { resolve } from 'path'
-// import react from '@vitejs/plugin-react' // Or your framework plugin
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    htmlInject() // <-- This should now work
+    htmlInject()
   ],
-  // plugins: [react()], // Or your framework plugin
-  base: '/<repository-name>/', // <--- UPDATE THIS LINE
+  base: '/',
   server: {
-    open: true, // Automatically open the browser
+    open: true,
     port: 3000
   },
   build: {
@@ -20,10 +17,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         installation: resolve(__dirname, 'installation.html'),
         imprint: resolve(__dirname, 'imprint.html'),
-        // Add other HTML files here if needed
       },
     },
-  // outDir defaults to 'dist', which matches your workflow
-  // outDir: 'dist',
   }
 }) 
